8f31a2266e3c908ab3409eaa927c1d3af7a4f792
Rafael Barbosa <rafaelsouzas153@gmail.com>
2026-06-05 19:58:12 -0300

    Update base code

--- kernel/task.c
+++ kernel/task.c
@@ -1,33 +1,24 @@
-#include "task.h"
-#include "memory.h"
+#pragma once
 #include <stdint.h>
 
-TCB tasks[MAX_TASKS];
-int task_count = 0;
+#define MAX_TASKS 8
+#define DEFAULT_STACK_SIZE 2048
+
+typedef struct
+{
+    uint64_t regs[31];   // x1–x31 (x0 é zero)
+    uint64_t sepc;
+
+    void (*entry)(void);
+
+    int priority;
+    uint8_t *stack;
+
+} TCB;
+
+extern TCB tasks[MAX_TASKS];
+extern int task_count;
 
 void xTaskCreate(void (*task)(void),
                  uint32_t stack_size,
-                 int priority)
-{
-    if (task_count >= MAX_TASKS)
-        return;
-
-    if (stack_size == 0)
-        stack_size = DEFAULT_STACK_SIZE;
-
-    TCB *t = &tasks[task_count];
-
-    t->stack = (uint8_t*)kmalloc(stack_size);
-
-    uint64_t *sp = (uint64_t*)(t->stack + stack_size);
-
-    /* Configurar contexto inicial */
-
-    t->regs[0] = (uint64_t)task;   // ra
-    t->regs[1] = (uint64_t)sp;     // sp
-
-    t->entry = task;
-    t->priority = priority;
-
-    task_count++;
-}
+                 int priority);
