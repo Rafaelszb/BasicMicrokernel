7123b3d8bfab357b4486eb82192348050d5bf8ac
Rafael Barbosa <rafaelsouzas153@gmail.com>
2026-06-09 17:12:14 -0300

    Update base code

--- kernel/scheduler.c
+++ kernel/scheduler.c
@@ -8,15 +8,14 @@ extern void context_switch(void*, void*);
 
 static int current = 0;
 
-/*   Round-Robin padrão   */
-
+/* Round-Robin padrão   */
 static int round_robin()
 {
+    if (task_count <= 1) return 0;
     return (current + 1) % task_count;
 }
 
-/*   Algoritmo atual   */
-
+/* Algoritmo atual   */
 static sched_algo_t current_algo = round_robin;
 
 void scheduler_set_algorithm(sched_algo_t algo)
@@ -25,40 +24,44 @@ void scheduler_set_algorithm(sched_algo_t algo)
         current_algo = algo;
 }
 
-
 void schedule_from_trap(uint64_t *frame)
 {
     int prev = current;
     int next = current_algo();
-/* TODO:
-* Copiar frame -> tasks[prev].regs*/
+
+    // Proteção: Se for a mesma tarefa, não há necessidade de trocar contexto
+    if (prev == next) {
+        return;
+    }
+
+    /* Copiar frame -> tasks[prev].regs (Limite corrigido para 31) */
     for(int i = 0; i < 31; i++){
         tasks[prev].regs[i] = frame[i];
-}
-/* TODO:
-* Salvar sepc da task atual.*/
+    }
+
+    /* Salvar sepc da task atual. */
     uint64_t current_sepc;
     asm volatile("csrr %0, sepc" : "=r"(current_sepc));
     tasks[prev].sepc = current_sepc;
     
     current = next;
-/* TODO:
-* Copiar tasks[next].regs -> frame*/
+
+    /* Copiar tasks[next].regs -> frame */
     for(int i = 0; i < 31; i++){
         frame[i] = tasks[next].regs[i];
     }
-/* TODO:
-* Restaurar sepc da prxima task.*/
+
+    /* Restaurar sepc da próxima task. */
     asm volatile("csrw sepc, %0" :: "r"(tasks[next].sepc));    
 }
 
-
-/*   Yield   */
-
+/* Yield   */
 void yield()
 {
     int prev = current;
     int next = current_algo();
+
+    if (prev == next) return;
 
     current = next;
 
@@ -66,12 +69,12 @@ void yield()
                    tasks[next].regs);
 }
  
-/*   Início   */
-
+/* Início   */
 void scheduler_start()
 {
     if (task_count == 0)
         return;
 
+    current = 0;
     tasks[0].entry();
 }
