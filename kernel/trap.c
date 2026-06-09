7982d94cce040ee935c5105c1a0386f9a69107c8
Rafael Barbosa <rafaelsouzas153@gmail.com>
2026-06-09 18:56:39 -0300

    Update base code

--- kernel/trap.c
+++ kernel/trap.c
@@ -10,26 +10,24 @@
 
 void trap_handler(uint64_t *frame)
 {
-  uint64_t scause;
-  /* TODO:
-  * Ler CSR scause.*/
-  asm volatile("csrr %0, scause" : "=r"(scause));
-  /* TODO:
-  * Verificar se interrupo. */
-  if(scause & SCAUSE_INTERRUPT_BIT){
-    uint64_t code = scause & SCAUSE_CODE_MASK;
-  /* TODO:
-  * Identificar Supervisor Timer Interrupt.  */
-    if(code == IRQ_S_TIMER){
-    /* TODO:
-    * Chamar timer_next().
-    * Chamar schedule_from_trap(frame).*/      
-      timer_next();
-      schedule_from_trap(frame);
-      return;
+    uint64_t scause;
+    // Lê o motivo do trap
+    asm volatile("csrr %0, scause" : "=r"(scause));
+
+    // Verifica se o bit mais significativo é 1 (Interrupção) 
+    // e se o código é 5 (Supervisor Timer Interrupt)
+    if ((scause & (1ULL << 63)) && ((scause & 0xFFF) == 5)) {
+        
+        // 1. Agenda o próximo tick do relógio para limpar a flag de interrupção do hardware
+        timer_next();
+
+        // 2. Chama o escalonador para trocar as pilhas e o sepc dentro do frame
+        schedule_from_trap(frame);
+        
+    } else {
+        // Se caiu aqui, foi um erro de instrução, página ou ecall não esperado
+        uart_print("Trap nao tratado! scause: ");
+        // (Opcional) Print do valor de scause em hexa para debug
+        while(1); 
     }
-  }
-  
-  uart_print("Unhandled trap\n");
-  while (1);
 }
