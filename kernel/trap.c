3ff20f36931fbced001cfa9dbd639713ebc603b2
Rafael Barbosa <rafaelsouzas153@gmail.com>
2026-06-09 22:13:31 -0300

    Update base code

--- kernel/trap.c
+++ kernel/trap.c
@@ -3,11 +3,6 @@
 #include "scheduler.h"
 #include "uart.h"
 
-#define SCAUSE_INTERRUPT_BIT (1ULL << 63)
-#define SCAUSE_CODE_MASK (~(1ULL << 63))
-
-#define IRQ_S_TIMER 5
-
 extern void uart_print(const char*);
 
 void trap_handler(uint64_t *frame)
@@ -30,17 +25,8 @@ void trap_handler(uint64_t *frame)
         // 2. Transfere o controle para o escalonador rotacionar as tarefas
         schedule_from_trap(frame);
         
-    } else {
-        // Se caiu aqui, vamos travar, mas avisando qual o número do problema
-        uart_print("\n[Kernel] Trap nao tratado! scause de debug: ");
-        
-        // Se o código for 1, significa um Instruction Misaligned Fault
-        // Se is_interrupt for 0 e trap_code for 9, é uma ecall vinda do U-mode
-        if (is_interrupt == 0) {
-            uart_print("Excecao sincrona codigo ");
-            // Como dica visual simples, você pode deduzir se é falha de página, etc.
-        }
-        
-        while(1); 
-    }
+    } 
+    
+    uart_print("Unhandled trap\n");
+    while (1);
 }
