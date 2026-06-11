0d443026dcdbf7246d9488dc188e3a73da6839cc
Rafael Barbosa <rafaelsouzas153@gmail.com>
2026-06-10 21:39:11 -0300

    Update base code

--- kernel/trap.c
+++ kernel/trap.c
@@ -7,22 +7,26 @@ extern void uart_print(const char*);
 
 void trap_handler(uint64_t *frame)
 {
-    uint64_t scause_val;
+    uint64_t scause;
     
-    // Lê o CSR scause
-    asm volatile("csrr %0, scause" : "=r"(scause_val));
+/* TODO: Ler CSR scause.*/
+
+    asm volatile(
+        "csrr %0, scause" 
+        : "=r"(scause)
+    );
 
     // Isolamos o bit 63 (Interrupt) e os 4 bits inferiores (Exception Code)
-    uint64_t is_interrupt = (scause_val >> 63) & 1ULL;
-    uint64_t trap_code = scause_val & 0xFFFULL;
+    uint64_t is_interrupt = (scause >> 63) & 1ULL;
+    uint64_t trap_code = scause & 0xFFFULL;
 
-    // Se for uma interrupção (1) E o código for do Timer de Supervisor (5)
-    if (is_interrupt && (trap_code == 5)) {
+    /* TODO: Verificar se interrupo.*/
+    if (is_interrupt && (trap_code == 5)) { /* TODO: Identificar Supervisor Timer Interrupt.*/
         
-        // 1. Limpa a interrupção atual agendando o próximo tick
+     /* TODO: Chamar timer_next().*/
         timer_next();
 
-        // 2. Transfere o controle para o escalonador rotacionar as tarefas
+    /*Chamar schedule_from_trap(frame).*/
         schedule_from_trap(frame);
         return;
     } 
